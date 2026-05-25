d69e9c8627ed06347d5cba06343af8f6a54cdf75
thimmegowda <thimmegowdahn2008@gmail.com>
2026-05-25 15:57:50 +0530

    Added requirements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     try {
 
       const response = await axios.post(
-        "https://ai-code-reviewer-backend.onrender.com",
+        "https://ai-code-reviewer-backend.onrender.com/review",
         {
           code: code,
         }
